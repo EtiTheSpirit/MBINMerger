0e3c79d429ba3277a5646cdffba3538c49398044
XanTheDragon <subzeroxan@gmail.com>
2019-09-09 00:41:26 -0500

    Fix navigation bug, change output desc

---
+++ Loader/MultiMBINEnumerator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libMBIN;
+using MBINMerger.XanEXMLRepresentation;
+
+namespace MBINMerger.Loader {
+
+	/// <summary>
+	/// Represents multiple MBIN files at once. They will all be loaded alongside eachother for comparison.<para/>
+	/// This class specifically stores the vanilla MBIN file, and all of the MBIN files that should be merged together.
+	/// </summary>
+	public class MultiMBINEnumerator {
+
+		private EXmlBase VanillaEXML;
+		private string VanillaPath;
+		private List<EXmlBase> ModEXMLs;
+		private List<string> ModPaths;
+
+		private ParentedEXmlObject VanillaEXMLParented;
+		private List<ParentedEXmlObject> ModEXMLsParented;
+
+		/// <summary>
+		/// Construct a new parallel MBIN file.
+		/// </summary>
+		/// <param name="sourceFile">The vanilla version of this MBIN file.</param>
+		/// <param name="filesToMerge">A list of modified MBIN files that should have their contents merged.</param>
+		/// <param name="vanillaPath">The path to the vanilla MBIN file.</param>
+		/// <param name="mergePaths">The paths to all of the files being merged.</param>
+		public MultiMBINEnumerator(MBINFile sourceFile, IEnumerable<MBINFile> filesToMerge, string vanillaPath, IEnumerable<string> mergePaths) {
+			VanillaEXML = sourceFile.GetData().SerializeEXml(false);
+			VanillaEXMLParented = ParentedEXmlObject.TransformEntireElementTree(VanillaEXML);
+			VanillaPath = vanillaPath;
+			ModEXMLs = new List<EXmlBase>(filesToMerge.Count());
+			ModEXMLsParented = new List<ParentedEXmlObject>(filesToMerge.Count());
+			ModPaths = mergePaths.ToList();
+			foreach (MBINFile file in filesToMerge) {
+				EXmlBase data = file.GetData().SerializeEXml(false);
+				ModEXMLs.Add(data);
+				ModEXMLsParented.Add(ParentedEXmlObject.TransformEntireElementTree(data));
+			}
+		}
+
+		/// <summary>
+		/// Patches all of the data, then returns the patched data.
+		/// </summary>
+		public EXmlData PatchEverything() {
+			// Set up a list of conflicting MBIN files.
+			List<ParentedEXmlObject> conflictingValues = new List<ParentedEXmlObject>();
+			List<string> conflictingPaths = new List<string>();
+			int currentFiles = 0;
+
+			foreach (ParentedEXmlObject obj in VanillaEXMLParented.Descendants) {
+				conflictingValues.Clear();
+
+				// Go through all of the values in the vanilla MBIN...
+				if (obj.IsLowestChildObject) {
+					// If the object has no children, it's likely a property. So...
+
+					for (int idx = 0; idx < ModEXMLsParented.Count; idx++) {
+						ParentedEXmlObject moddedObj = ModEXMLsParented[idx];
+						// Go through all of the modded EXMLs and get the corresponding property in *their* data...
+						ParentedEXmlObject dataContainer = moddedObj.GetObjectFromPath(obj.Path);
+						if (dataContainer != null) {
+							// And once we have the data within the modded MBIN, we check if it's different.
+							if (obj.Attribute.Value != dataContainer.Attribute.Value) {
+								// And if it's different, add that to the conflicting list (if there ends up being only one item in this list, then there is no actual conflict and we just use the value)
+								conflictingValues.Add(dataContainer);
+								conflictingPaths.Add(ModPaths[idx]);
+							}
+
+						} else {
+							// The data should NEVER be null if the MBIN files are from the same source. Throw an error.
+							throw new Exception(string.Format("Could not locate {0} in a modded MBIN file! Do these MBIN files all modify the same vanilla file?", obj.Path));
+						}
+					}
+				}
+
+				int targetReplacement = 0;
+				if (conflictingValues.Count > 1) {
+					Console.WriteLine(); // Bump it down for the counter.
+					targetReplacement = ResolveFileConflict(obj, conflictingPaths.ToArray(), conflictingValues.ToArray());
+				}
+
+				if (targetReplacement != -1 && conflictingValues.Count != 0) {
+					obj.Attribute.Value = conflictingValues[targetReplacement].Attribute.Value;
+				}
+				currentFiles++;
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.Write("Processed {0} MBIN properties (of {1}) in {2} files.", currentFiles, VanillaEXMLParented.Descendants.Count, ModPaths.Count);
+				Console.CursorLeft = 0;
+			}
+			
+			Console.WriteLine();
+			return (EXmlData)VanillaEXMLParented.ConvertToSerializableEXML();
+		}
+
+		/// <summary>
+		/// Prompts the user whenever there is a conflict between multiple files.
+		/// </summary>
+		/// <param name="data">The EXML node storing the affected data.</param>
+		/// <param name="conflictingFiles">The list of files that conflict eachother.</param>
+		/// <returns></returns>
+		private int ResolveFileConflict(ParentedEXmlObject data, string[] conflictingFiles, ParentedEXmlObject[] objects) {
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Multiple MBIN files overwrite {0}! Would you like to keep...", data.Path);
+			for (int idx = 0; idx < conflictingFiles.Length; idx++) {
+				ParentedEXmlObject targetObject = objects[idx];
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.Write("Value #{0}: ", idx + 1);
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.Write(conflictingFiles[idx]);
+				Console.ForegroundColor = ConsoleColor.Magenta;
+				if (targetObject.Attribute != null) {
+					Console.WriteLine(" [{0}=\"{1}\"]", targetObject.Attribute.AttributeName, targetObject.Attribute.Value);
+				} else {
+					Console.WriteLine(" [UNKNOWN VALUE - ERROR WHEN READING DATA]");
+				}
+			}
+		ENTER_NUMBER:
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("Enter a number and press enter. ");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Enter 0 to keep the vanilla property from the stock MBIN file.");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("> ");
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			string numStr = Console.ReadLine();
+			if (int.TryParse(numStr, out int selection)) {
+				selection--; // They are prompted a 1-index, we need a 0-index
+				if (selection < -1 || selection >= conflictingFiles.Length) {
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("ERROR: The number you input is too small or too large!");
+					goto ENTER_NUMBER;
+				}
+				return selection;
+
+			}
+			else {
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR: Unable to convert your input into a number!");
+				goto ENTER_NUMBER;
+			}
+		}
+
+	}
+}
---
+++ XanEXMLRepresentation/ParentedEXmlObject.cs
@@ -0,0 +1,320 @@
+﻿using libMBIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBINMerger.XanEXMLRepresentation {
+
+	/// <summary>
+	/// A custom implementation of <seealso cref="libMBIN.EXmlBase"/> with some extra information with the point of building a customized data tree, such as a Parent property as well as methods to construct a text-based path and a method of getting a specific child via a path.
+	/// </summary>
+	public class ParentedEXmlObject : EXmlBase {
+
+		/// <summary>Stores the parent object.</summary>
+
+		private ParentedEXmlObject ParentInternal = null;
+		/// <summary>
+		/// Represents the parent object of this element. A null parent signifies that this is the root element.
+		/// </summary>
+		public ParentedEXmlObject Parent {
+			get {
+				return ParentInternal;
+			}
+			set {
+				ParentInternal = value;
+				PathCached = null; // Tells the Path property to regenerate the path since the parent has changed.
+			}
+		}
+
+		/// <summary>
+		/// True if this object has no children. If this is true, this object is likely storing a property that the modder is intending to change. If it is false, this is likely something that contains properties within it.
+		/// </summary>
+		public bool IsLowestChildObject {
+			get {
+				return Children.Count == 0;
+			}
+		}
+
+		/// <summary>
+		/// The attribute this ExmlObject has. See the docs on <seealso cref="EXmlAttribute"/> for more information. This is intended to remove the need to perform trial and error when casting to a subclass.<para/>
+		/// Will be null if this ExmlObject has no attributes on it.
+		/// </summary>
+		public EXmlAttribute Attribute { get; private set; }
+
+		/// <summary>
+		/// Represents the children of this ParentedEXmlObject. Please reference <seealso cref="EXmlBase.Elements"/> when serializing this object.<para/>
+		/// WARNING: THIS ARRAY IS NOT SYNCHRONIZED WITH THE ELEMENTS ARRAY. Any edits made to one of the two will not be reflected to the other!
+		/// </summary>
+		public List<ParentedEXmlObject> Children { get; }
+
+
+		private List<ParentedEXmlObject> DescendantsInternal = null;
+		/// <summary>
+		/// Represents the descendants of this ParentedEXmlObject. Descendants refers to "recursive children". This is a list of everything that is a child of this object, either directly or indirectly (e.g. child of a child)
+		/// </summary>
+		public IReadOnlyList<ParentedEXmlObject> Descendants {
+			get {
+				if (DescendantsInternal == null) {
+					DescendantsInternal = new List<ParentedEXmlObject>();
+					PopulateDescendantsInternal(this);
+				}
+				return DescendantsInternal.AsReadOnly();
+			}
+		}
+
+		/// <summary>A cached, internal value for the Path. This prevents repeatedly generating a path and improves performance.</summary>
+		private string PathCached = null;
+
+		/// <summary>
+		/// The path to this object that can be used to navigate through the EXML tree structure.
+		/// </summary>
+		/// <returns></returns>
+		public string Path {
+			get {
+				if (PathCached != null) {
+					return PathCached;
+				}
+				List<string> objects = new List<string>();
+				objects.Add(Name);
+				ParentedEXmlObject parent = Parent;
+				while (parent != null) {
+					if (parent.Name != null) {
+						objects.Add(parent.Name);
+					}
+					else {
+						if (parent.Attribute.AttributeName == "template") {
+							// This is the root node.
+							objects.Add("EXML");
+						}
+						else {
+							// Catch case: Some objects may have an attribute and no name. This generally happens when there's a certain object represented within the EXML which is everywhere.
+							if (parent.Attribute != null) {
+								// Since data containers have .xml extensions, I need to strip it of its .xml
+								string val = parent.Attribute.Value;
+								val = val.Replace(".xml", "");
+								objects.Add(string.Format("DataContainer[{0}]", val));
+							} else {
+								// There's no attribute and no name. This is not good! This should never occur under normal execution.
+								throw new Exception("An EXML node was skimmed that had no attributes within it!");
+							}
+						}
+					}
+					parent = parent.Parent;
+				}
+
+				// Reverse the table since if we print it as-is, it'll be backwards (current child => root, we want root => current child)
+				objects.Reverse();
+				string retVal = "";
+				foreach (string data in objects) {
+					retVal += data;
+					if (data != objects.Last()) {
+						retVal += ".";
+					}
+				}
+				PathCached = retVal;
+				return retVal;
+			}
+		}
+
+		/// <summary>
+		/// Construct a new ParentedEXmlObject from an existing Base.
+		/// </summary>
+		/// <param name="baseObject">The existing base.</param>
+		protected ParentedEXmlObject(EXmlBase baseObject) {
+			Name = baseObject.Name;
+			Elements = baseObject.Elements;
+			Children = new List<ParentedEXmlObject>();
+			EXmlData data = baseObject as EXmlData;
+			EXmlMeta meta = baseObject as EXmlMeta;
+			EXmlProperty prop = baseObject as EXmlProperty;
+			if (data != null) {
+				Attribute = new EXmlAttribute {
+					AttributeName = "template",
+					Value = data.Template
+				};
+			} else if (meta != null) {
+				Attribute = new EXmlAttribute {
+					AttributeName = "comment",
+					Value = meta.Comment,
+				};
+			} else if (prop != null) {
+				Attribute = new EXmlAttribute {
+					AttributeName = "value",
+					Value = prop.Value
+				};
+			} else {
+				Attribute = null;
+			}
+		}
+
+		/// <summary>
+		/// Returns a ParentedEXmlObject within the specified parent that can be found with the specified path.
+		/// </summary>
+		/// <param name="parent">The parent EXML node.</param>
+		/// <param name="path">The path to the desired node.</param>
+		/// <returns></returns>
+		public ParentedEXmlObject GetObjectFromParentAndPath(ParentedEXmlObject parent, string path) {
+			if (parent == null || path == null) return null;
+
+			string[] pathElements = path.Split('.');
+			if (pathElements.Length > 1) {
+				pathElements = pathElements.Skip(1).ToArray();
+			}
+			else {
+				return null;
+			}
+
+			foreach (string target in pathElements) {
+				if (target.StartsWith("DataContainer[")) {
+					// This is a custom container with no name. Don't search by name.
+					string valueName = target.Replace("DataContainer[", "");
+					valueName = valueName.Substring(0, valueName.Length - 1); // Cuts off the ending ]
+					foreach (ParentedEXmlObject child in parent.Children) {
+						if (child.Attribute.Value.Replace(".xml", "") == valueName) {
+							parent = child; // Update the parent and then break the loop.
+							break;
+						}
+					}
+				}
+				else {
+					// Normal value.
+					foreach (ParentedEXmlObject child in parent.Children) {
+						if (child.Name == target) {
+							parent = child; // Update the parent and then break the loop.
+							break;
+						}
+					}
+				}
+			}
+
+			// Lastly, check if we got the right child. If we got lost somewhere in the tree, the name of the child won't be the same as the last path element.
+			string lastElement = pathElements.Last();
+			string intendedChildName = null;
+			if (lastElement.StartsWith("DataContainer[")) {
+				// It's a container. Strip the garbage.
+				intendedChildName = parent.Attribute.Value.Replace(".xml", "");
+				lastElement = lastElement.Replace("DataContainer[", "");
+				lastElement = lastElement.Substring(0, intendedChildName.Length - 1);
+			}
+			else {
+				intendedChildName = parent.Name;
+			}
+
+			if (lastElement == intendedChildName) return parent;
+
+			// Report the error.
+			ConsoleColor oldColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Unable to find child from path {0} (last child was {1})", path, parent.Name);
+			Console.ForegroundColor = oldColor;
+			return null;
+		}
+
+		/// <summary>
+		/// Returns a ParentedEXmlObject within this object that can be found with the specified path.
+		/// </summary>
+		/// <param name="path">The path to the desired node.</param>
+		/// <returns></returns>
+		public ParentedEXmlObject GetObjectFromPath(string path) {
+			return GetObjectFromParentAndPath(this, path);
+		}
+
+		private void PopulateDescendantsInternal(ParentedEXmlObject currentScannedParent) {
+			foreach (ParentedEXmlObject obj in currentScannedParent.Children) {
+				DescendantsInternal.Add(obj);
+				PopulateDescendantsInternal(obj);
+			}
+		}
+
+		/// <summary>
+		/// Converts this custom object back into an EXmlBase so that it can be serialized back into an MBIN.
+		/// </summary>
+		/// <returns></returns>
+		public EXmlBase ConvertToSerializableEXML(EXmlBase parent = null, ParentedEXmlObject parentAsNative = null) {
+
+			EXmlBase root;
+			if (parent == null) {
+				// Attribute is gonna be template. If it's not, there's a problem.
+				if (Attribute.AttributeName != "template") {
+					throw new Exception("Top level attribute is not a template!");
+				}
+				EXmlData newData = new EXmlData();
+				newData.Name = Name;
+				newData.Template = Attribute.Value;
+				root = newData;
+			} else {
+				root = parent;
+			}
+
+			//EXmlBase root = parent ?? this;
+			parentAsNative = parentAsNative ?? this;
+			root.Elements = new List<EXmlBase>();
+			foreach (ParentedEXmlObject child in parentAsNative.Children) {
+				EXmlBase childAsBase = child;
+				if (child.Attribute?.AttributeName == "template") {
+					EXmlData childAsTyped = new EXmlData();
+					childAsTyped.Name = childAsBase.Name;
+					childAsTyped.Elements = childAsBase.Elements;
+					childAsTyped.Template = child.Attribute.Value;
+					root.Elements.Add(childAsTyped);
+					childAsBase = childAsTyped;
+
+				} else if (child.Attribute?.AttributeName == "value") {
+					EXmlProperty childAsTyped = new EXmlProperty();
+					childAsTyped.Name = childAsBase.Name;
+					childAsTyped.Elements = childAsBase.Elements;
+					childAsTyped.Value = child.Attribute.Value;
+					root.Elements.Add(childAsTyped);
+					childAsBase = childAsTyped;
+
+				} else if (child.Attribute?.AttributeName == "comment") {
+					EXmlMeta childAsTyped = new EXmlMeta();
+					childAsTyped.Name = childAsBase.Name;
+					childAsTyped.Elements = childAsBase.Elements;
+					childAsTyped.Comment = child.Attribute.Value;
+					root.Elements.Add(childAsTyped);
+					childAsBase = childAsTyped;
+
+				} else {
+					root.Elements.Add(childAsBase);
+				}
+
+				ConvertToSerializableEXML(childAsBase, child);
+			}
+			return root;
+		}
+
+		/// <summary>
+		/// Converts an EXmlBase and all of its child elements into an XXMLBase. Do not implicitly or explicitly cast EXmlBase into XXMLObject as it will not populate the parent data.
+		/// </summary>
+		/// <param name="obj">The EXmlBase to convert.</param>
+		/// <param name="parent">The parent object of the XXMLObject. If you are converting an EXmlBase manually, it is advised that you do not specify this.</param>
+		public static ParentedEXmlObject TransformEntireElementTree(EXmlBase obj, ParentedEXmlObject parent = null) {
+			ParentedEXmlObject root = new ParentedEXmlObject(obj);
+			root.Parent = parent;
+			foreach (EXmlBase exml in obj.Elements) {
+				root.Children.Add(TransformEntireElementTree(exml, root));
+			}
+			return root;
+		}
+	}
+
+	/// <summary>
+	/// Represents an attribute within an EXmlBase's subtypes (e.g. "template", "value", etc)
+	/// </summary>
+	public class EXmlAttribute {
+
+		/// <summary>
+		/// The name of the attribute (e.g. "template" or "value")
+		/// </summary>
+		public string AttributeName { get; set; }
+
+		/// <summary>
+		/// The value of this attribute.
+		/// </summary>
+		public string Value { get; set; }
+
+	}
+}
